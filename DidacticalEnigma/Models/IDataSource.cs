7c070009d80188c441916e9e71cf9c05afdfdc2d
milleniumbug <milleniumbug@protonmail.com>
2018-08-04 18:50:05 -0700

    Add IDataSource

---
+++ DidacticalEnigma/Models/IDataSource.cs
@@ -0,0 +1,42 @@
+﻿using DidacticalEnigma.Utils;
+using System;
+using System.Collections.Async;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace DidacticalEnigma.Models
+{
+    interface IDataSource
+    {
+        IAsyncEnumerable<FlowDocument> Answer(
+            Request request,
+            CancellationToken cancellation = default(CancellationToken));
+
+        Task<UpdateResult> UpdateLocalDataSource(
+            CancellationToken cancellation = default(CancellationToken));
+    }
+    public class Request
+    {
+        public string QueryText { get; }
+
+        public PartOfSpeech PartOfSpeech { get; }
+
+        public Request(string queryText, PartOfSpeech partOfSpeech = PartOfSpeech.Unknown)
+        {
+            QueryText = queryText;
+            PartOfSpeech = partOfSpeech;
+        }
+    }
+
+    public enum UpdateResult
+    {
+        NoUpdateNeeded,
+        Updated,
+        Failure,
+        Cancelled
+    }
+}
