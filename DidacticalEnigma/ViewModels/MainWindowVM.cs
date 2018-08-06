669ac4d5d376be11c40873deff2bada339b6dd03
milleniumbug <milleniumbug@protonmail.com>
2018-08-06 15:35:52 -0700

    Finishing touches

--- DidacticalEnigma/ViewModels/MainWindowVM.cs
+++ DidacticalEnigma/ViewModels/MainWindowVM.cs
@@ -17,12 +17,76 @@ using DidacticalEnigma.ViewModels;
 
 namespace DidacticalEnigma
 {
+    public class TextBufferVM : INotifyPropertyChanged
+    {
+        public ObservableBatchCollection<LineVM> Lines { get; } = new ObservableBatchCollection<LineVM>();
+
+        private string rawOutput = "";
+
+        private readonly ILanguageService lang;
+
+        public string RawOutput
+        {
+            get => rawOutput;
+            set
+            {
+                if (rawOutput == value)
+                    return;
+                rawOutput = value;
+                OnPropertyChanged();
+                SetAnnotations(value);
+            }
+        }
+
+        private SelectionInfoVM selectionInfo;
+        public SelectionInfoVM SelectionInfo
+        {
+            get => selectionInfo;
+
+            set
+            {
+                if (selectionInfo == value)
+                    return;
+                selectionInfo = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Name { get; }
+
+        private void SetAnnotations(string unannotatedOutput)
+        {
+            Lines.Clear();
+            Lines.AddRange(
+                lang.BreakIntoSentences(unannotatedOutput)
+                    .Select(sentence => new LineVM(sentence.Select(word => new WordVM(word, lang)))));
+            RawOutput = string.Join(
+                "\n",
+                Lines.Select(
+                    line => string.Join(
+                        " ",
+                        line.Words.Select(word => word.StringForm))));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public TextBufferVM(string name, ILanguageService lang)
+        {
+            this.lang = lang;
+            Name = name;
+        }
+    }
 
     public class MainWindowVM : INotifyPropertyChanged, IDisposable
     {
         private readonly ILanguageService lang;
-        private readonly JMDict jdict;
-        private ClipboardHook hook;
+
+        private readonly ClipboardHook hook;
 
         public string AboutText => File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"dic\about.txt"), Encoding.UTF8);
 
@@ -61,98 +125,51 @@ namespace DidacticalEnigma
             HiraganaBoard = new KanaBoardVM(Path.Combine(baseDir, @"dic\hiragana_romaji.txt"), Encoding.UTF8, lang);
             KatakanaBoard = new KanaBoardVM(Path.Combine(baseDir, @"dic\katakana_romaji.txt"), Encoding.UTF8, lang);
             UsageDataSourceVM = new UsageDataSourcePreviewVM(lang, Path.Combine(baseDir, "dic"));
-            Update = new RelayCommand(() =>
-            {
-                SetAnnotations(Input);
-            }, () =>
-            {
-                return string.IsNullOrEmpty(RawOutput);
-            });
-            Reset = new RelayCommand(() =>
-            {
-                RawOutput = "";
-                SetAnnotations("");
-            }, () =>
-            {
-                return !string.IsNullOrEmpty(RawOutput);
-            });
-            PlaceInClipboard = new RelayCommand((p) =>
-            {
-                var codePoint = (CodePoint)p;
-                Clipboard.SetText(codePoint.ToString());
-            });
+            TextBuffers.Add(new TextBufferVM("Scratchpad", lang));
+            TextBuffers.Add(new TextBufferVM("Main", lang));
+            ClipboardTextBuffer = new TextBufferVM("Clipboard", lang);
+            KanjiLookupVM = new KanjiRadicalLookupControlVM(lang);
             hook = new ClipboardHook();
             hook.ClipboardChanged += SetContent;
-            KanjiLookupVM = new KanjiRadicalLookupControlVM(lang);
+            SendToCurrent = new RelayCommand(() =>
+            {
+                if (CurrentTextBuffer == null)
+                    return;
+                CurrentTextBuffer.RawOutput = ClipboardTextBuffer.RawOutput;
+            });
+            SendToScratchpad = new RelayCommand(() =>
+            {
+                TextBuffers[0].RawOutput = ClipboardTextBuffer.RawOutput;
+            });
         }
 
         private void SetContent(object sender, string e)
         {
-            Input = e;
+            ClipboardTextBuffer.RawOutput = e;
         }
 
-        private void SetAnnotations(string unannotatedOutput)
+        public ObservableBatchCollection<TextBufferVM> TextBuffers { get; } = new ObservableBatchCollection<TextBufferVM>();
+
+        private TextBufferVM currentTextBuffer;
+        public TextBufferVM CurrentTextBuffer
         {
-            Lines.Clear();
-            Lines.AddRange(
-                lang.BreakIntoSentences(unannotatedOutput)
-                    .Select(sentence => new LineVM(sentence.Select(word => new WordVM(word, lang)))));
-            RawOutput = string.Join(
-                "\n",
-                Lines.Select(
-                    line => string.Join(
-                        " ",
-                        line.Words.Select(word => word.StringForm))));
-        }
+            get => currentTextBuffer;
 
-        private string input = "";
-
-        public ObservableBatchCollection<LineVM> Lines { get; } = new ObservableBatchCollection<LineVM>();
-
-        private string rawOutput = "";
-        public string RawOutput
-        {
-            get => rawOutput;
             set
             {
-                if (rawOutput == value)
+                if (currentTextBuffer == value)
                     return;
-                rawOutput = value;
-                OnPropertyChanged();
-                Update.OnExecuteChanged();
-                Reset.OnExecuteChanged();
-                SetAnnotations(value);
-            }
-        }
-        public string Input
-        {
-            get => input;
-
-            set
-            {
-                input = value;
+                currentTextBuffer = value;
                 OnPropertyChanged();
             }
         }
 
-        private SelectionInfoVM selectionInfo;
-        public SelectionInfoVM SelectionInfo
-        {
-            get => selectionInfo;
-
-            set
-            {
-                if (selectionInfo == value)
-                    return;
-                selectionInfo = value;
-                OnPropertyChanged();
-            }
-        }
+        public TextBufferVM ClipboardTextBuffer { get; }
 
         public RelayCommand PlaceInClipboard { get; }
 
-        public RelayCommand Update { get; }
-        public RelayCommand Reset { get; }
+        public RelayCommand SendToScratchpad { get; }
+        public RelayCommand SendToCurrent { get; }
 
         private IEnumerable<string> SplitWords(string input)
         {
@@ -187,10 +204,8 @@ namespace DidacticalEnigma
 
         public void Dispose()
         {
-            hook.Dispose();
             UsageDataSourceVM.Dispose();
             lang.Dispose();
-            jdict.Dispose();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
