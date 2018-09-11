9248a08c3546a17968416b9393194bb0e1839181
milleniumbug <milleniumbug@protonmail.com>
2018-09-10 19:56:52 -0700

    Oops forgot to commit these

---
+++ JDict/FrequencyList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace JDict
+{
+    public class FrequencyList
+    {
+        private readonly Dictionary<string, double> frequency;
+
+        public FrequencyList(string path, Encoding encoding)
+        {
+            this.frequency = File.ReadLines(path, encoding)
+                .Skip(4)
+                .Where(line => line != "")
+                .Select(line => line.Split(' '))
+                .ToDictionary(i => i[2], i => double.Parse(i[1], CultureInfo.InvariantCulture));
+        }
+
+        public double RateFrequency(string s)
+        {
+            if (frequency.TryGetValue(s, out var value))
+            {
+                return value;
+            }
+            return double.NegativeInfinity;
+        }
+    }
+}
