7b9c454b009b30caecac34715e82afbe64fcac15
milleniumbug <milleniumbug@protonmail.com>
2018-10-19 21:31:46 +0200

    Helper application to export Zero-Epwing's JSON bitmap font dumps as png

---
+++ EpwingRemapper.Core/CodeIdentifier.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace EpwingRemapper.Core
+{
+    public class CodeIdentifier : IEquatable<CodeIdentifier>
+    {
+        public CharacterKind Kind { get; }
+
+        public int Code { get; }
+
+        public int SubBookIndex { get; }
+
+        public CodeIdentifier(CharacterKind kind, int code, int subBookIndex)
+        {
+            Kind = kind;
+            Code = code;
+            SubBookIndex = subBookIndex;
+        }
+
+        public bool Equals(CodeIdentifier other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Kind == other.Kind && Code == other.Code && SubBookIndex == other.SubBookIndex;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((CodeIdentifier) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (int) Kind;
+                hashCode = (hashCode * 397) ^ Code;
+                hashCode = (hashCode * 397) ^ SubBookIndex;
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(CodeIdentifier left, CodeIdentifier right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(CodeIdentifier left, CodeIdentifier right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
--- EpwingRemapper.Core/Program.cs
+++ EpwingRemapper.Core/Program.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Primitives;
 
 namespace EpwingRemapper.Core
 {
@@ -7,34 +14,55 @@ namespace EpwingRemapper.Core
     {
         public static int Main(string[] args)
         {
-            var config = new Configuration();
-            using (var e = (args as IEnumerable<string>).GetEnumerator())
+            try
             {
-                while (e.MoveNext())
+                var config = new Configuration();
+                using (var e = (args as IEnumerable<string>).GetEnumerator())
                 {
-                    switch (e.Current?.Trim())
+                    while (e.MoveNext())
                     {
-                        case "-o":
-                        case "--output":
-                            ReadOutputDir(config, e);
-                            break;
-                        case "-i":
-                        case "--bitmap-json-data":
-                            ReadCapture2TextCliExecutablePath(config, e);
-                            break;
-                        case "-c2t":
-                        case "--capture2text-path":
-                            ReadInputBitmapJson(config, e);
-                            break;
-                        case "-h":
-                        case "--help":
-                            DisplayHelp();
-                            return 0;
+                        switch (e.Current?.Trim())
+                        {
+                            case "-o":
+                            case "--output":
+                                ReadOutputDir(config, e);
+                                break;
+                            case "-i":
+                            case "--bitmap-json-data":
+                                ReadInputBitmapJson(config, e);
+                                break;
+                            case "-c2t":
+                            case "--capture2text-path":
+                                ReadCapture2TextCliExecutablePath(config, e);
+                                break;
+                            case "-h":
+                            case "--help":
+                                DisplayHelp();
+                                return 0;
+                        }
                     }
                 }
-            }
 
-            return Main(config);
+                var _ = ProcessFonts<Rgb24>(config, (img, id) =>
+                {
+                    var targetDir = Directory
+                        .CreateDirectory(Path.Combine(config.OutputDirectory, id.SubBookIndex.ToString())).FullName;
+                    using (var targetPng =
+                        File.OpenWrite(Path.Combine(targetDir,
+                            $"{id.Kind}_{id.Code}_{img.Width}x{img.Height}.png")))
+                    {
+                        img.SaveAsPng(targetPng);
+                    }
+
+                    return null;
+                });
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
 
             void ReadOutputDir(Configuration cfg, IEnumerator<string> e)
             {
@@ -66,14 +94,74 @@ namespace EpwingRemapper.Core
             }
         }
 
-        public static int Main(Configuration config)
+        public static Dictionary<CodeIdentifier, List<KeyValuePair<Size, string>>> ProcessFonts<TPixel>(Configuration config, CharacterRecognizer<TPixel> recognizer)
+            where TPixel : struct, IPixel<TPixel>
         {
             config.Validate();
 
+            var result = new Dictionary<CodeIdentifier, List<KeyValuePair<Size, string>>>();
+            using (var jsonFile = File.OpenText(config.InputBitmapJson))
+            using (var jsonReader = new JsonTextReader(jsonFile))
+            {
+                var serializer = new JsonSerializer();
+                var root = serializer.Deserialize<Root>(jsonReader);
+                foreach (var (subbook, subbookindex) in root.Subbooks.Select((s, i) => (s, i)))
+                {
+                    foreach (var font in subbook.Fonts)
+                    {
+                        var narrow = font.Narrow.AllGlyphs
+                            .Select(g => (CharacterKind.Narrow, g, (width: font.Narrow.Width, height: font.Narrow.Height)));
+                        var wide = font.Wide.AllGlyphs
+                            .Select(g => (CharacterKind.Wide, g, (width: font.Wide.Width, height: font.Wide.Height)));
+                        foreach (var (kind, glyph, (width, height)) in narrow.Concat(wide))
+                        {
+                            var expanded = glyph.BitmapData.SelectMany(p => new[]
+                            {
+                                (p & 128) == 0 ? NamedColors<TPixel>.White : NamedColors<TPixel>.Black,
+                                (p & 64) == 0 ? NamedColors<TPixel>.White : NamedColors<TPixel>.Black,
+                                (p & 32) == 0 ? NamedColors<TPixel>.White : NamedColors<TPixel>.Black,
+                                (p & 16) == 0 ? NamedColors<TPixel>.White : NamedColors<TPixel>.Black,
+                                (p & 8) == 0 ? NamedColors<TPixel>.White : NamedColors<TPixel>.Black,
+                                (p & 4) == 0 ? NamedColors<TPixel>.White : NamedColors<TPixel>.Black,
+                                (p & 2) == 0 ? NamedColors<TPixel>.White : NamedColors<TPixel>.Black,
+                                (p & 1) == 0 ? NamedColors<TPixel>.White : NamedColors<TPixel>.Black,
+                            }).ToArray();
+                            var img = Image.LoadPixelData(expanded, width, height);
+                            var recognitionAttempt = recognizer(img, new  CodeIdentifier(kind, glyph.Code, subbookindex));
+                            var value = GetOrAdd(result, new CodeIdentifier(kind, glyph.Code, subbookindex), () => new List<KeyValuePair<Size, string>>());
+                            value.Add(new KeyValuePair<Size, string>(new Size(width, height), recognitionAttempt));
+                        }
+                    }
+                }
+            }
+            return result;
+        }
 
-            return 0;
+        private static TValue GetOrAdd<TKey, TValue>(IDictionary<TKey, TValue> dict, TKey key, Func<TValue> Valuefactory)
+        {
+            if (dict.TryGetValue(key, out var value))
+            {
+                return value;
+            }
+            else
+            {
+                value = Valuefactory();
+                dict[key] = value;
+                return value;
+            }
         }
     }
+
+    public enum CharacterKind
+    {
+        Narrow,
+        Wide
+    }
+
+    public delegate string CharacterRecognizer<TPixel>(
+        Image<TPixel> img,
+        CodeIdentifier id)
+        where TPixel : struct, IPixel<TPixel>;
 
     public class Configuration
     {
@@ -88,7 +176,7 @@ namespace EpwingRemapper.Core
 
         internal void Validate()
         {
-            if(Capture2TextCliExecutablePath == null)
+            if (Capture2TextCliExecutablePath == null)
                 throw new ArgumentException("this argument is mandatory", nameof(Capture2TextCliExecutablePath));
 
             if (OutputDirectory == null)
@@ -98,6 +186,62 @@ namespace EpwingRemapper.Core
                 throw new ArgumentException("this argument is mandatory", nameof(InputBitmapJson));
 
         }
+
+    }
+
+    internal class Root
+    {
+        public string CharCode { get; set; }
+
+        public string DiscCode { get; set; }
+
+        public IEnumerable<SubBook> Subbooks { get; set; }
+    }
+
+    internal class SubBook
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("copyright")]
+        public string Copyright { get; set; }
+
+        [JsonProperty("fonts")]
+        public IEnumerable<Font> Fonts { get; set; }
+    }
+
+    internal class Font
+    {
+        [JsonProperty("narrow")]
+        public Glyphs Narrow { get; set; }
+
+        [JsonProperty("wide")]
+        public Glyphs Wide { get; set; }
+    }
+
+    internal class Glyphs
+    {
+        [JsonProperty("glyphs")]
+        public List<Glyph> AllGlyphs { get; set; }
+
+        [JsonProperty("width")]
+        public int Width { get; set; }
+
+        [JsonProperty("height")]
+        public int Height { get; set; }
+    }
+
+    internal class Glyph
+    {
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        [JsonProperty("bitmap")]
+        public byte[] BitmapData { get; set; }
+
+        [JsonProperty("translatedCodePoint")]
+        public int? CodePoint { get; set; }
+
 
     }
 }
