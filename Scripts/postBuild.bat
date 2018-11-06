e89cfa231030a1d69d172ae3188ae5bbe7161b5e
milleniumbug <milleniumbug@protonmail.com>
2018-11-06 18:21:16 +0100

    Only copy required files

--- Scripts/postBuild.bat
+++ Scripts/postBuild.bat
@@ -4,4 +4,5 @@ Set DataDir=%2
 REM s - Copies directories and subdirectories except empty ones.
 REM I - If the destination does not exist and copying more than one file, assumes that destination must be a directory.
 REM Y - Suppresses prompting to confirm you want to overwrite an existing destination file.
-xcopy /s /I /Y %DataDir% %OutputDir%
+REM D - copy only files newer than the destination
+xcopy /s /I /Y /D %DataDir% %OutputDir%
