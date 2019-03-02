d04dbe4a39485191f3cc08758a983e4b2c220668
milleniumbug <milleniumbug@protonmail.com>
2019-03-02 17:43:18 +0100

    Add performance tests

--- JDict.Tests/PerformanceTests.cs
+++ JDict.Tests/PerformanceTests.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DidacticalEnigma.Core.Models.LanguageService;
 using JetBrains.dotMemoryUnit;
 using NUnit.Framework;
 
@@ -12,6 +13,23 @@ namespace JDict.Tests
 {
     class PerformanceTests
     {
+        private KanjiDict kanjiDict;
+        private JDict.Kradfile kradfile;
+        private Radkfile radkfile;
+        private KanjiProperties kanjiProperties;
+        private CodePoint[] radicalCodePoint;
+        private IKanjiOrdering ordering;
+
+        public PerformanceTests()
+        {
+            kanjiDict = JDict.KanjiDict.Create(TestDataPaths.KanjiDic);
+            kradfile = new JDict.Kradfile(TestDataPaths.Kradfile, Encoding.UTF8);
+            radkfile = new Radkfile(TestDataPaths.Radkfile, Encoding.UTF8);
+            kanjiProperties = new KanjiProperties(kanjiDict, kradfile, radkfile, new RadicalRemapper(kradfile, radkfile));
+            radicalCodePoint = radicalCodePoint = new[]{CodePoint.FromString("一") };
+            ordering = kanjiProperties.KanjiOrderings.First();
+        }
+
         [Explicit]
         [Test]
         public void Performance()
@@ -39,6 +57,15 @@ namespace JDict.Tests
                 File.Delete(cache);
             }
             
+        }
+
+        [Explicit]
+        [Test]
+        public void RadicalLookup()
+        {
+            var watch = Stopwatch.StartNew();
+            _ = kanjiProperties.LookupKanjiByRadicals(radicalCodePoint, ordering).ToList();
+            Assert.Less(watch.Elapsed, TimeSpan.FromMilliseconds(1));
         }
     }
 }
