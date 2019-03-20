aa2e3f6c13df7dd4f33af68369efbcef358558f3
milleniumbug <milleniumbug@protonmail.com>
2019-03-20 23:55:21 +0100

    Replace polling clipboard hook with event-based clipboard hook

--- DidacticalEnigma/Utils/ClipboardHook.cs
+++ DidacticalEnigma/Utils/ClipboardHook.cs
@@ -1,53 +1,85 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Interop;
 using System.Windows.Threading;
 
 namespace DidacticalEnigma.Utils
 {
-    // Not actually a clipboard hook
-    // This does polling every now and then instead
-    // TODO: make a proper implementation
-    class ClipboardHook : IDisposable
+    // based on
+    // https://codereview.stackexchange.com/q/115417/25793
+    internal sealed class ClipboardHook : IDisposable
     {
-        private string previous = "";
+        private static class NativeMethods
+        {
+            /// <summary>
+            /// Places the given window in the system-maintained clipboard format listener list.
+            /// </summary>
+            [DllImport("user32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool AddClipboardFormatListener(IntPtr hwnd);
 
-        private DispatcherTimer timer;
+            /// <summary>
+            /// Removes the given window from the system-maintained clipboard format listener list.
+            /// </summary>
+            [DllImport("user32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
 
-        public event EventHandler<string> ClipboardChanged;
+            /// <summary>
+            /// Sent when the contents of the clipboard have changed.
+            /// </summary>
+            public const int WM_CLIPBOARDUPDATE = 0x031D;
+
+            /// <summary>
+            /// To find message-only windows, specify HWND_MESSAGE in the hwndParent parameter of the FindWindowEx function.
+            /// </summary>
+            public static IntPtr HWND_MESSAGE = new IntPtr(-3);
+        }
+
+        private bool disposed = false;
+
+        private HwndSource hwndSource = new HwndSource(0, 0, 0, 0, 0, 0, 0, null, NativeMethods.HWND_MESSAGE);
 
         public ClipboardHook()
         {
-            timer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Normal, TimerCallback, Dispatcher.CurrentDispatcher);
-            timer.Start();
-        }
-
-        private void TimerCallback(object sender, EventArgs e)
-        {
-            try
-            {
-                RaiseClipboardChanged(Clipboard.GetText());
-            }
-            catch (COMException)
-            {
-                // trying to silence "data in clipboard is invalid" exception
-                // we simply won't update our clipboard in that case
-            }
+            hwndSource.AddHook(WndProc);
+            NativeMethods.AddClipboardFormatListener(hwndSource.Handle);
         }
 
         public void Dispose()
         {
-            timer.Stop();
+            if (disposed)
+                return;
+
+            disposed = true;
+            NativeMethods.RemoveClipboardFormatListener(hwndSource.Handle);
+            hwndSource.RemoveHook(WndProc);
+            hwndSource.Dispose();
         }
 
-        protected void RaiseClipboardChanged(string newData)
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
-            if (newData == "")
-                return;
-            if (previous == newData)
-                return;
-            previous = newData;
-            ClipboardChanged?.Invoke(this, newData);
+            if (msg == NativeMethods.WM_CLIPBOARDUPDATE)
+            {
+                try
+                {
+                    var text = Clipboard.GetText();
+                    ClipboardChanged?.Invoke(this, text);
+                }
+                catch (COMException)
+                {
+                    // trying to silence "data in clipboard is invalid" exception
+                    // we simply won't update our clipboard in that case
+                }
+            }
+
+            return IntPtr.Zero;
         }
+
+        /// <summary>
+        /// Occurs when the clipboard content changes.
+        /// </summary>
+        public event EventHandler<string> ClipboardChanged;
     }
 }
