3b2b336fda4cb90d2d68189fa1482a0412992a5c
milleniumbug <milleniumbug@protonmail.com>
2019-03-27 00:12:38 +0100

    Add test for KanjiDict

---
+++ JDict.Tests/KanjiDictTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DidacticalEnigma.Core.Models.LanguageService;
+using NUnit.Framework;
+
+namespace JDict.Tests
+{
+    [TestFixture]
+    class KanjiDictTests
+    {
+        [Test]
+        public void Test()
+        {
+            var kanjiDict = KanjiDict.Create(TestDataPaths.KanjiDic);
+            var entry = kanjiDict.Lookup("南");
+            entry.Match(e =>
+            {
+                CollectionAssert.AreEqual(new[]{"みなみ"}, e.KunReadings);
+                Assert.AreEqual(e.FrequencyRating, 341);
+            }, () =>
+            {
+                Assert.Fail();
+            });
+        }
+    }
+}
