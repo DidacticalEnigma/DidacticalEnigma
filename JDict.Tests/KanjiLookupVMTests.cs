7441be1e264d1cf414dd2593ffdb6480102c6408
milleniumbug <milleniumbug@protonmail.com>
2019-03-26 04:41:14 +0100

    Add failing test

---
+++ JDict.Tests/KanjiLookupVMTests.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using DidacticalEnigma.Core.Models.LanguageService;
+using DidacticalEnigma.ViewModels;
+using NUnit.Framework;
+
+namespace JDict.Tests
+{
+    [RequiresThread(ApartmentState.STA)]
+    [TestFixture]
+    class KanjiLookupVMTests
+    {
+        private static KanjiRadicalLookup lookup;
+        private static KanjiDict kanjiDict;
+        private static IKanjiProperties kanjiProperties;
+
+        private KanjiRadicalLookupControlVM vm;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            kanjiDict = KanjiDict.Create(TestDataPaths.KanjiDic);
+            using (var reader = File.OpenText(TestDataPaths.Radkfile))
+            {
+                lookup = new KanjiRadicalLookup(Radkfile.Parse(reader), kanjiDict);
+            }
+            kanjiProperties = new KanjiProperties(
+                kanjiDict,
+                new JDict.Kradfile(TestDataPaths.Kradfile, Encoding.UTF8),
+                new Radkfile(TestDataPaths.Radkfile, Encoding.UTF8),
+                null);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            vm = new KanjiRadicalLookupControlVM(lookup, kanjiProperties);
+        }
+
+        [Test]
+        public void Test()
+        {
+            var x = vm.Radicals.First(r => r.CodePoint == CodePoint.FromInt('个'));
+            var y = vm.Radicals.First(r => r.CodePoint == CodePoint.FromInt('ハ'));
+            var z = vm.Radicals.First(r => r.CodePoint == CodePoint.FromInt('乃'));
+            x.Selected = true;
+            y.Selected = true;
+            Assert.False(z.Enabled);
+        }
+    }
+}
