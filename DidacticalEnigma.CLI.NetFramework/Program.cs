b1b3114a42c1a89a1aa1c19c4982ba73cee33e5d
milleniumbug <milleniumbug@protonmail.com>
2019-04-15 21:47:06 +0200

    Add CLI projects

---
+++ DidacticalEnigma.CLI.NetFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using DidacticalEnigma.CLI.Common;
+
+namespace DidacticalEnigma.CLI.NetFramework
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            return EntryPoint.Main(args);
+        }
+    }
+}
