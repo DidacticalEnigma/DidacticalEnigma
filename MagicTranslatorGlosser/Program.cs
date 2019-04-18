7d84a4d240a2424cd4308bfb93c4a58b67948316
milleniumbug <milleniumbug@protonmail.com>
2019-04-19 01:18:44 +0200

    Explicitly set output encoding to UTF8

--- MagicTranslatorGlosser/Program.cs
+++ MagicTranslatorGlosser/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 using DidacticalEnigma.CLI.Common;
 
 namespace MagicTranslatorGlosser
@@ -9,6 +10,7 @@ namespace MagicTranslatorGlosser
     {
         static int Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             return EntryPoint.Main(new []{ Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "Data"), "autoglosser" }.Concat(args).ToArray());
         }
     }
